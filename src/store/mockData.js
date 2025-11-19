cdd64eaead316a86c48c9ba7050bb8f2682c4f6b
zhangzhang88 <68057668+zhangzhang88@users.noreply.github.com>
2025-11-19 17:52:29 +0800

    Update mockData.js

--- src/store/mockData.js
+++ src/store/mockData.js
@@ -1,10 +1,10 @@
 export const mockArticles = [
     {
         id: 1,
-        title: "Steve Jobs Speech",
+        title: "Part 1 :Mike Is a Cook",
         language: "English",
         level: "Intermediate",
-        content: "I am honored to be with you today at your commencement from one of the finest universities in the world.",
+        content: "Mike gets up at six am every morning.\n\nHe makes breakfast and drinks a coffee.\n\nHe drives to work in his car.\n\nHis work starts at seven thirty am.\n\nMike is a cook at a restaurant.\n\nHe makes food for hungry customers.\n\nThe customers are from many countries.\n\nThey speak many different languages.\n\nMike can meet many friendly people.\n\nMike is happy when he talks to the customers.",
         image: "https://placehold.co/600x400?text=Steve+Jobs"
     }
 ];
